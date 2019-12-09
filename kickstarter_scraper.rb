27052404efb213077c35f4781c4633535d42d339
Whittney Davis <wcdavis89@yahoo.com>
2019-12-09 15:56:00 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -6,5 +6,8 @@ def create_project_hash
   kickstarter = Nokogiri::HTML(html)
   binding.pry
 end
+
+# projects: kickstarter.css("li.project.grid_4")
+# title: project.css("h2.bbcard_name strong a").text
  
 create_project_hash
