7979f799140c81d3c38c57a7058fe04ca2cb9a05
Whittney Davis <wcdavis89@yahoo.com>
2019-12-09 16:43:44 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -16,7 +16,9 @@ def create_project_hash
  
   # Iterate through the projects
   kickstarter.css("li.project.grid_4").each do |project|
-    projects[project] = {}
+    title = project.css("h2.bbcard_name strong a").text
+    projects[title.to_sym] = {}
+    #to_sym creates symbol for hash
   end
  
   # return the projects hash
